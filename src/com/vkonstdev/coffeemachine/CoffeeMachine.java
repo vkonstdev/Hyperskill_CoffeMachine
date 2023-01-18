2ef1e8aa7b66e73a51da3d5564af7a10e6dfee67
vkonstdev <vkonstdev@gmail.com>
2023-01-18 13:28:27 +0700

    Stage 6/6 complete. Refactoring

--- src/com/vkonstdev/coffeemachine/CoffeeMachine.java
+++ src/com/vkonstdev/coffeemachine/CoffeeMachine.java
@@ -60,25 +60,6 @@ public class CoffeeMachine {
         this.moneyQuantity = moneyQuantity;
     }
 
-    /*private int calculateAvailableCups() {
-        return min(waterQuantity / WATER_PER_CUP, milkQuantity / MILK_PER_CUP, coffeeQuantity / COFFEE_PER_CUP);
-    }*/
-
-    /*private int min(int a, int b, int c) {
-        return (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
-    }*/
-
-    /*public void printMessage(int countCups) {
-        int availableCups = calculateAvailableCups();
-        if (countCups == availableCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (countCups < availableCups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", availableCups - countCups);
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee\n", availableCups);
-        }
-    }*/
-
     public void printStateOfCoffeeMachine() {
         System.out.println("\nThe coffee machine has:");
         System.out.printf("%d ml of water\n", waterQuantity);
--- src/com/vkonstdev/coffeemachine/Main.java
+++ src/com/vkonstdev/coffeemachine/Main.java
@@ -3,20 +3,9 @@ package com.vkonstdev.coffeemachine;
 public class Main {
 
     public static void main(String[] args) {
-        /*Scanner sc = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = sc.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = sc.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee = sc.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int countCups = sc.nextInt();*/
         CoffeeMachine cm = new CoffeeMachine(400, 540, 120, 9, 550);
         while (true) {
             cm.chooseOperation();
         }
-        //cm.printStateOfCoffeeMachine();
-
     }
 }
