09d532ec2ddcecbb1935c943d6b5e96085cce6aa
vkonstdev <vkonstdev@gmail.com>
2023-01-18 10:50:00 +0700

    Stage 4/6 complete

--- src/com/vkonstdev/coffeemachine/CoffeeMachine.java
+++ src/com/vkonstdev/coffeemachine/CoffeeMachine.java
@@ -1,19 +1,23 @@
 package com.vkonstdev.coffeemachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
-    public static final int WATER_PER_CUP = 200;
-    public static final int MILK_PER_CUP = 50;
-    public static final int COFFEE_PER_CUP = 15;
+    public static final Scanner sc = new Scanner(System.in);
 
     private int waterQuantity;
     private int milkQuantity;
     private int coffeeQuantity;
+    private int cupsQuantity;
+    private int moneyQuantity;
 
-    public CoffeeMachine(int waterQuantity, int milkQuantity, int coffeeQuantity) {
+    public CoffeeMachine(int waterQuantity, int milkQuantity, int coffeeQuantity, int cupsQuantity, int moneyQuantity) {
         this.waterQuantity = waterQuantity;
         this.milkQuantity = milkQuantity;
         this.coffeeQuantity = coffeeQuantity;
+        this.cupsQuantity = cupsQuantity;
+        this.moneyQuantity = moneyQuantity;
     }
 
     public int getWaterQuantity() {
@@ -40,21 +44,31 @@ public class CoffeeMachine {
         this.coffeeQuantity = coffeeQuantity;
     }
 
-    /*public void printMessage(int countCups) {
-        System.out.printf("For %d cups of coffee you will need:\n", countCups);
-        System.out.printf("%d ml of water\n", countCups * WATER_PER_CUP);
-        System.out.printf("%d ml of milk\n", countCups * MILK_PER_CUP);
-        System.out.printf("%d g of coffee beans\n", countCups * COFFEE_PER_CUP);
-    }*/
-    private int calculateAvailableCups() {
+    public int getCupsQuantity() {
+        return cupsQuantity;
+    }
+
+    public void setCupsQuantity(int cupsQuantity) {
+        this.cupsQuantity = cupsQuantity;
+    }
+
+    public int getMoneyQuantity() {
+        return moneyQuantity;
+    }
+
+    public void setMoneyQuantity(int moneyQuantity) {
+        this.moneyQuantity = moneyQuantity;
+    }
+
+    /*private int calculateAvailableCups() {
         return min(waterQuantity / WATER_PER_CUP, milkQuantity / MILK_PER_CUP, coffeeQuantity / COFFEE_PER_CUP);
-    }
+    }*/
 
-    private int min(int a, int b, int c) {
+    /*private int min(int a, int b, int c) {
         return (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
-    }
+    }*/
 
-    public void printMessage(int countCups) {
+    /*public void printMessage(int countCups) {
         int availableCups = calculateAvailableCups();
         if (countCups == availableCups) {
             System.out.println("Yes, I can make that amount of coffee");
@@ -62,6 +76,90 @@ public class CoffeeMachine {
             System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", availableCups - countCups);
         } else {
             System.out.printf("No, I can make only %d cup(s) of coffee\n", availableCups);
+        }
+    }*/
+
+    public void printStateOfCoffeeMachine() {
+        System.out.println("\nThe coffee machine has:");
+        System.out.printf("%d ml of water\n", waterQuantity);
+        System.out.printf("%d ml of milk\n", milkQuantity);
+        System.out.printf("%d g of coffee beans\n", coffeeQuantity);
+        System.out.printf("%d disposable cups\n", cupsQuantity);
+        System.out.printf("$%d of money\n", moneyQuantity);
+        System.out.println();
+    }
+
+    public void fill() {
+        System.out.println("Write how many ml of water you want to add:");
+        int water = sc.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        int milk = sc.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        int coffee = sc.nextInt();
+        System.out.println("Write how many disposable cups you want to add:");
+        int cups = sc.nextInt();
+        addIngredients(water, milk, coffee, cups);
+    }
+
+    private void addIngredients(int water, int milk, int coffee, int cups) {
+        waterQuantity += water;
+        milkQuantity += milk;
+        coffeeQuantity += coffee;
+        cupsQuantity += cups;
+    }
+
+    private void makeCupOfCoffee(Coffee cof) {
+        waterQuantity -= cof.water;
+        milkQuantity -= cof.milk;
+        coffeeQuantity -= cof.coffee;
+        moneyQuantity += cof.cost;
+        cupsQuantity--;
+    }
+
+    public void take() {
+        if (moneyQuantity > 0) {
+            System.out.println("I gave you $" + moneyQuantity);
+            moneyQuantity = 0;
+        }
+    }
+
+    public void buy() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        Coffee cof;
+        switch(sc.nextInt()) {
+            case 1 -> cof = Coffee.ESPRESSO;
+            case 2 -> cof = Coffee.LATTE;
+            case 3 -> cof = Coffee.CAPPUCCINO;
+            default -> throw new IllegalStateException("Unexpected value: " + sc.nextInt());
+        }
+        makeCupOfCoffee(cof);
+    }
+
+    public void chooseOperation() {
+        System.out.println("Write action (buy, fill, take):");
+        String operation = sc.nextLine();
+        switch (operation) {
+            case "buy" -> buy();
+            case "fill" -> fill();
+            case "take" -> take();
+        }
+    }
+
+    private enum Coffee {
+        ESPRESSO(250, 0, 16, 4),
+        LATTE(350, 75, 20, 7),
+        CAPPUCCINO(200, 100, 12, 6);
+
+        private final int water;
+        private final int milk;
+        private final int coffee;
+        private final int cost;
+
+        Coffee(int water, int milk, int coffee, int cost) {
+            this.water = water;
+            this.milk = milk;
+            this.coffee = coffee;
+            this.cost = cost;
         }
     }
 }
--- src/com/vkonstdev/coffeemachine/Main.java
+++ src/com/vkonstdev/coffeemachine/Main.java
@@ -1,11 +1,9 @@
 package com.vkonstdev.coffeemachine;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         System.out.println("Write how many ml of water the coffee machine has:");
         int water = sc.nextInt();
         System.out.println("Write how many ml of milk the coffee machine has:");
@@ -13,8 +11,11 @@ public class Main {
         System.out.println("Write how many grams of coffee beans the coffee machine has:");
         int coffee = sc.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
-        int countCups = sc.nextInt();
-        CoffeeMachine cm = new CoffeeMachine(water, milk, coffee);
-        cm.printMessage(countCups);
+        int countCups = sc.nextInt();*/
+        CoffeeMachine cm = new CoffeeMachine(400, 540, 120, 9, 550);
+        cm.printStateOfCoffeeMachine();
+        cm.chooseOperation();
+        cm.printStateOfCoffeeMachine();
+
     }
 }
